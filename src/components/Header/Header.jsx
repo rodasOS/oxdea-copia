d6cffc4e9d19bdaa4dff039da5b457fb6a68d751
rodasOS <139723305+rodasOS@users.noreply.github.com>
2024-03-23 12:32:50 -0600

    Agregando el primer Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css';
+
+export function Header() {
+	return (
+		<header>
+			<div>
+				<h3>Teléfono: 433276</h3>
+			</div>
+			<div>
+				<i>1</i>
+				<i>2</i>
+				<i>3</i>
+				<i>4</i>
+			</div>
+			<div>sección 3</div>
+		</header>
+	);
+}
